bbca2f29692e8b93b7e02a1fe3b11b2c6d4fc60c
Jesse Terblanche <jessenterblanche@gmail.com>
2021-08-26 15:23:50 +0200

    update

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class Database(object):
         self.conn.row_factory = dict_factory
         self.cursor = self.conn.cursor()
 
-    def register(self, data):
+    def register(self, data, image):
         values = data
         put_data = {}
         put_data['email'] = values.get('email')
@@ -28,9 +28,8 @@ class Database(object):
         put_data['username'] = values.get('username')
         put_data['password'] = values.get('password')
         put_data['tag'] = values.get('tag')
-        put_data['profile_image'] = values.get('profile_image')
         put_data['is_active'] = values.get('is_active')
-        if values.get("profile_image") is not None:
+        if image.get('profile_image') is not None:
             self.cursor.execute("INSERT INTO user("
                                 "email,"
                                 "full_name,"
@@ -38,13 +37,18 @@ class Database(object):
                                 "password,"
                                 "tag,"
                                 "profile_image,"
-                                "is_active) VALUES(?, ?, ?)", (put_data['email'],
-                                                               put_data['full_name'],
-                                                               put_data['username'],
-                                                               put_data['password'],
-                                                               put_data['tag'],
-                                                               put_data['profile_image'],
-                                                               put_data['is_active']))
+                                "is_active) VALUES(?, ?, ?, ?, ?, ?, ?)", (put_data['email'],
+                                                                           put_data['full_name'],
+                                                                           put_data['username'],
+                                                                           put_data['password'],
+                                                                           put_data['tag'],
+                                                                           image_convert(),
+                                                                           put_data['is_active']))
+            mail = Mail(app)
+
+            msg = Message('Registration Successful', sender='lottoemail123@gmail.com', recipients=[put_data['email']])
+            msg.body = "Welcome to 'this app'."
+            mail.send(msg)
         else:
             self.cursor.execute("INSERT INTO user("
                                 "email,"
@@ -52,64 +56,95 @@ class Database(object):
                                 "username,"
                                 "password,"
                                 "tag,"
-                                "is_active) VALUES(?, ?, ?)", (put_data['email'],
-                                                               put_data['full_name'],
-                                                               put_data['username'],
-                                                               put_data['password'],
-                                                               put_data['tag'],
-                                                               put_data['is_active']))
+                                "is_active) VALUES(?, ?, ?, ?, ?, ?)", (put_data['email'],
+                                                                        put_data['full_name'],
+                                                                        put_data['username'],
+                                                                        put_data['password'],
+                                                                        put_data['tag'],
+                                                                        put_data['is_active']))
+            mail = Mail(app)
+
+            msg = Message('Registration Successful', sender='lottoemail123@gmail.com', recipients=[put_data['email']])
+            msg.body = "Welcome to 'this app'."
+            mail.send(msg)
 
     def login(self, email, password):
         self.cursor.execute("SELECT * FROM user WHERE email=? AND password=?", (email, password,))
         return self.cursor.fetchone()
 
-    def showallusers(self):
+    def show_all_users(self):
         self.cursor.execute("SELECT * FROM user")
         return self.cursor.fetchall()
 
-    def edituser(self, email, value):
-        email = email
-        values = value
-        query = "UPDATE user SET first_name=?, last_name=?, address=?, username=?, password=? WHERE email='" + email + "'"
-        self.cursor.execute(query, values)
+    def select_user(self, userid):
+        self.cursor.execute("SELECT * FROM user WHERE userId='" + str(userid) + "'")
+        return self.cursor.fetchone()
 
-    def selectproduct(self, value):
-        proid = value
-        query = "SELECT * FROM catalogue WHERE product_id='" + proid + "'"
-        self.cursor.execute(query)
-        data = self.cursor.fetchall()
-        return data
+    def edit_user(self, userid, data, image):
+        pimage = image
+        values = data
+        put_data = {}
 
-    def myproducts(self, value):
-        email = value
-        query = "SELECT * FROM catalogue WHERE email='" + email + "'"
-        self.cursor.execute(query)
-        data = self.cursor.fetchall()
-        return data
+        if values.get('email') is not None:
+            put_data['email'] = values.get('email')
+            self.cursor.execute("INSERT INTO user(email) VALUES(?) WHERE userId='" + str(userid) + "'",
+                                (put_data['email']))
+            self.conn.commit()
 
-    def viewcat(self):
-        self.cursor.execute("SELECT * FROM catalogue")
-        data = self.cursor.fetchall()
-        return data
+        if values.get('full_name') is not None:
+            put_data['full_name'] = values.get('full_name')
+            self.cursor.execute("INSERT INTO user(full_name) VALUES(?) WHERE userId='" + str(userid) + "'",
+                                (put_data['full_name']))
+            self.conn.commit()
 
-    def delete_user(self, email):
-        self.cursor.execute("DELETE FROM user WHERE email='" + email + "'")
-        self.conn.commit()
+        if values.get('username') is not None:
+            put_data['username'] = values.get('username')
+            self.cursor.execute("INSERT INTO user(username) VALUES(?) WHERE userId='" + str(userid) + "'",
+                                (put_data['username']))
+            self.conn.commit()
+
+        if values.get('password') is not None:
+            put_data['password'] = values.get('password')
+            self.cursor.execute("INSERT INTO user(password) VALUES(?) WHERE userId='" + str(userid) + "'",
+                                (put_data['password']))
+            self.conn.commit()
+
+        if values.get('tag') is not None:
+            put_data['tag'] = values.get('tag')
+            self.cursor.execute("INSERT INTO user(tag) VALUES(?) WHERE userId='" + str(userid) + "'",
+                                (put_data['tag']))
+            self.conn.commit()
+
+        if values.get('bio') is not None:
+            put_data['bio'] = values.get('bio')
+            self.cursor.execute("INSERT INTO user(bio) VALUES(?) WHERE userId='" + str(userid) + "'",
+                                (put_data['bio']))
+            self.conn.commit()
+
+        if pimage.get('profile_image') is not None:
+            put_data['profile_image'] = pimage.get('profile_image')
+            self.cursor.execute("INSERT INTO user(profile_image) VALUES(?) WHERE userId='" + str(userid) + "'",
+                                (put_data['profile_image']))
+            self.conn.commit()
+
+
+
+    # def delete_user()
 
     def commit(self):
         return self.conn.commit()
 
 
-def upload_file():
+def image_convert():
     app.logger.info('in upload route')
     cloudinary.config(cloud_name ='dlqxdivje', api_key='599819111725767',
                       api_secret='lTD-aqaoTbzVgmZqyZxjPThyaVg')
     upload_result = None
     if request.method == 'POST' or request.method == 'PUT':
-        product_image = request.json['product_image']
-        app.logger.info('%s file_to_upload', product_image)
-        if product_image:
-            upload_result = cloudinary.uploader.upload(product_image)
+        profile_image = request.files['profile_image']
+        app.logger.info('%s file_to_upload', profile_image)
+        if profile_image:
+            upload_result = cloudinary.uploader.upload(profile_image)
             app.logger.info(upload_result)
             return upload_result['url']
 
@@ -120,12 +155,15 @@ def db_user_table():
     print("Opened database successfully")
 
     conn.execute("CREATE TABLE IF NOT EXISTS user(userId INTEGER PRIMARY KEY AUTOINCREMENT,"
-                 "email TEXT NOT NULL,"
+                 "email TEXT NOT NULL UNIQUE,"
                  "full_name TEXT NOT NULL,"
                  "username TEXT NOT NULL,"
                  "password TEXT NOT NULL,"
-                 "tag TEXT NOT NULL,"
+                 "tag TEXT NOT NULL UNIQUE,"
                  "profile_image TEXT,"
+                 "bio TEXT,"
+                 "followers TEXT,"
+                 "following TEXT,"
                  "is_active INTEGER NOT NULL)")
     print("user table created successfully")
     conn.close()
@@ -150,6 +188,7 @@ def db_posts_table():
 
     conn.execute("CREATE TABLE IF NOT EXISTS posts(postId INTEGER PRIMARY KEY AUTOINCREMENT,"
                  "userId TEXT NOT NULL,"
+                 "sourceId INTEGER NOT NULL,"
                  "text TEXT,"
                  "retweeted_by TEXT,"
                  "image1 TEXT,"
@@ -225,18 +264,16 @@ app.config['CORS_HEADERS'] = ['Content-Type']
 
 
 @app.route('/user/', methods=["POST", "GET", "PATCH"])
-def user_methods():
+def users_methods():
     response = {}
     dtb = Database()
 
-    # Fetches ALL users
     if request.method == "GET":
-        users = dtb.showallusers()
+        users = dtb.show_all_users()
         response['status_code'] = 200
         response['data'] = users
         return response
 
-    # LOGIN
     if request.method == "PATCH":
         email = request.json["email"]
         password = request.json["password"]
@@ -247,13 +284,33 @@ def user_methods():
         response['data'] = user
         return response
 
-    # REGISTER
     if request.method == "POST":
-        incoming_data = dict(request.json)
-        dtb.register(incoming_data)
+        incoming_data = dict(request.form)
+        image = dict(request.files)
+        dtb.register(incoming_data, image)
+        dtb.commit()
         response["message"] = "Success"
         response["status_code"] = 200
         return response
+
+
+@app.route('/user/<int:userid>/', methods=['POST', 'GET'])
+def user_methods(userid):
+    response = {}
+    dtb = Database()
+    if request.method == 'GET':
+        user = dtb.select_user(userid)
+        response['message'] = "Retrieved successfully"
+        response['data'] = user
+        return response
+
+    if request.method == 'PUT':
+        incoming_data = dict(request.form)
+        image = dict(request.files)
+        if incoming_data.get('following') is not None:
+            print('later')
+        else:
+            dtb.edit_user(userid, incoming_data, image)
 
 
 if __name__ == '__main__':
